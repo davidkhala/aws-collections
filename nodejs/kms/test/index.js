86701ab1e00a93a2a6b0a04d202fca2dd493697f
DavidLiu <david.yx.liu@oracle.com>
2021-07-30 15:21:31 +0800

    [DONE] kms sign and verify

--- nodejs/kms/test/index.js
+++ nodejs/kms/test/index.js
@@ -1,7 +1,6 @@
-
 const {KMS} = require('../kms');
 const assert = require('assert');
-const {Uint8Array2String, String2Uint8Array}= require('khala-light-util')
+const {Uint8Array2String, String2Uint8Array} = require('khala-light-util');
 describe('kms', () => {
 	const keyId = 'mrk-6b3d47c784234a37975d924aaadb54a5';
 	const kms = new KMS(keyId);
