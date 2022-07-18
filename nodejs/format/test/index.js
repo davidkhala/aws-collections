6c0affce2dcaf84ed8b9db9f8149e09e9bb267dc
David Liu <david.yx.liu@oracle.com>
2022-07-18 22:22:22 +0800

    CredentialsProviderError: Could not load credentials from any providers

--- nodejs/format/test/index.js
+++ nodejs/format/test/index.js
@@ -1,16 +1,11 @@
 import AWSClass from '../index.js';
-import {EC2Client} from '@aws-sdk/client-ec2';
 
-describe('ping', function () {
+describe('from cloud9 or instance principal', function () {
 	this.timeout(0);
-	const accessKeyId = 'AKIAY2KAWKW3ALRO7673';
-	const {secretAccessKey} = process.env;
 	const store = new AWSClass();
-	store.as(EC2Client);
 
 	it('ping with dry-run', async () => {
 		await store.ping();
-
 
 	});
 });
