b479ef551e4640d2c7a310f4a4c88e370c447fb8
David Liu <david.yx.liu@oracle.com>
2022-08-29 00:21:39 +0800

    Update cli.ps1

--- cli/cli.ps1
+++ cli/cli.ps1
@@ -5,7 +5,15 @@ function Install-CLI {
 function Get-Version {
     Get-AWSPowerShellVersion
 }
-function Configure-Profile {
+function Profile-Login {
+    # TODO 
+    Set-DefaultAWSRegion -Region ap-east-1
+
+    Set-AWSCredential -AccessKey $1 -SecretKey $2
+    Get-S3Bucket
+}
+function Session-Login {
+    # The credential profile that results from running this command wound not be stored locally
     Initialize-AWSDefaultConfiguration -Region ap-east-1
     Get-S3Bucket
 }
