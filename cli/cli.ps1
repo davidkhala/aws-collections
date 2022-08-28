6e7d35324acc83f0c959ec5929841fa52c257f32
David Liu <david.yx.liu@oracle.com>
2022-08-28 22:45:45 +0800

    Update cli.ps1

--- cli/cli.ps1
+++ cli/cli.ps1
@@ -2,3 +2,6 @@ function Install-CLI {
     Install-Module -name AWSPowerShell.NetCore -Force -Scope CurrentUser
     Import-Module AWSPowerShell.NetCore
 }
+function Get-Version{
+    Get-AWSPowerShellVersion
+}
