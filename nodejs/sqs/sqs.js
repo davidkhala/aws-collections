68d2e786c477cd494da2c7eadc4711d6ed9b4fad
davidliu <david-khala@hotmail.com>
2022-08-30 00:09:03 +0800

    #1 message timer

--- nodejs/sqs/sqs.js
+++ nodejs/sqs/sqs.js
@@ -1,26 +1,42 @@
 import AWSClass from '@davidkhala/aws-format';
-import {SQSClient, SendMessageCommand, CreateQueueCommand, ListQueuesCommand} from '@aws-sdk/client-sqs';
+import {
+	SQSClient,
+	SendMessageCommand,
+	CreateQueueCommand,
+	ListQueuesCommand,
+	GetQueueUrlCommand,
+	DeleteQueueCommand,
+	PurgeQueueCommand,
+	ReceiveMessageCommand,
+	DeleteMessageCommand,
+} from '@aws-sdk/client-sqs';
 
 export class SQS extends AWSClass {
 
 	constructor(param) {
 		super(param);
 		this.as(SQSClient);
+	}
 
+	async ping() {
+		const result = await super.ping();
+		// revert
+		this.as(SQSClient);
+		return result;
 	}
 
 	/**
 	 *
 	 * @param {string} QueueName
-	 * @param {boolean} FifoQueue Designates a queue as FIFO, When you set this attribute, you must also provide the `MessageGroupId` for your messages explicitly.
+	 * @param {boolean} [isFifo] Designates a queue as FIFO, When you set this attribute, you must also provide the `MessageGroupId` for your messages explicitly.
 	 * @return {Promise<String>} QueueUrl
 	 */
-	async create(QueueName, {FifoQueue} = {}) {
+	async create(QueueName, isFifo) {
 		const opts = {
 			QueueName,
 			Attributes: {}
 		};
-		if (FifoQueue) {
+		if (isFifo) {
 			opts.Attributes.FifoQueue = true;
 			opts.QueueName = `${QueueName}.fifo`;
 		}
@@ -34,27 +50,10 @@ export class SQS extends AWSClass {
 	 * @param {string} QueueName
 	 */
 	async getQueueUrl(QueueName) {
-		const {QueueUrl} = await this.sqs.getQueueUrl({QueueName}).promise();
+		const {QueueUrl} = await this.sendCommand({QueueName}, GetQueueUrlCommand);
 		return QueueUrl;
 	}
 
-	/**
-	 * Deletes the queue specified by the QueueUrl, regardless of the queue's contents.
-	 * @param {string} QueueUrl
-	 */
-	async destroy(QueueUrl) {
-		const opts = {QueueUrl};
-		await this.sqs.deleteQueue(opts).promise();
-	}
-
-	/**
-	 * Deletes the messages in a queue specified by the QueueURL parameter.
-	 * @param {string} QueueUrl
-	 */
-	async clean(QueueUrl) {
-		const opts = {QueueUrl};
-		await this.purgeQueue(opts).promise();
-	}
 
 	/**
 	 *
@@ -67,57 +66,81 @@ export class SQS extends AWSClass {
 		return QueueUrls;
 	}
 
+
+}
+
+export class Message extends AWSClass {
+	constructor(QueueUrl, param) {
+		super(param);
+		this.QueueUrl = QueueUrl;
+		this.as(SQSClient);
+	}
+
+	static FromSQS(QueueUrl, instance) {
+		return new Message(QueueUrl, Object.assign(instance.credentials, {region: instance.region}));
+	}
+
 	/**
-	 *
-	 * @param {string} QueueUrl
+	 * TODO MessageGroupId only work for fifo
 	 * @param {string} message
-	 * @param {object} extraOpts for attribute `DelaySeconds`, `MessageGroupId`
+	 * @param {number} [timeout] in milliseconds
 	 * @return {Promise<SQS.SendMessageResult>}
 	 */
-	async send(QueueUrl, message, extraOpts = {}) {
+	async send(message, timeout) {
 		const opts = {
 			MessageBody: message,
-			QueueUrl
+			QueueUrl: this.QueueUrl,
+			DelaySeconds: timeout / 1000,
 		};
-		Object.assign(opts, extraOpts);
-		return await this.sqs.sendMessage(opts).promise();
+		return await this.sendCommand(opts, SendMessageCommand);
 	}
 
 	/**
 	 *
-	 * @param {string} QueueUrl
-	 * @param {number} WaitTimeSeconds
-	 * @param {number} MaxNumberOfMessages
+	 * @param {string} ReceiptHandle An identifier associated with the act of receiving the message. A new receipt handle is returned every time you receive a message. When deleting a message, you provide the last received receipt handle to delete the message.
+	 */
+	async delete(ReceiptHandle) {
+		const opts = {
+			QueueUrl: this.QueueUrl,
+			ReceiptHandle
+		};
+
+		await this.sendCommand(opts, DeleteMessageCommand);
+	}
+
+	/**
+	 *
+	 * @param {number} numberOfMessages
+	 * @param {number} timeout
 	 * @return {Promise<SQS.Message[]>}
 	 */
-	async receive(QueueUrl, {WaitTimeSeconds, MaxNumberOfMessages} = {MaxNumberOfMessages: 1}) {
-		const opts = {QueueUrl};
-		if (WaitTimeSeconds && typeof WaitTimeSeconds === 'number') {
-			if (WaitTimeSeconds < 0 || WaitTimeSeconds > 20) {
-				throw Error(`Invalid WaitTimeSeconds ${WaitTimeSeconds}; Valid value should be an integer from 0 to 20 (seconds)`);
-			}
-			opts.WaitTimeSeconds = WaitTimeSeconds;
+	async receive({numberOfMessages = 1, timeout = 0} = {}) {
+		const opts = {QueueUrl: this.QueueUrl};
+		if (timeout && typeof timeout === 'number') {
+			opts.WaitTimeSeconds = timeout / 1000;
 		}
-		if (MaxNumberOfMessages && typeof MaxNumberOfMessages === 'number') {
-			opts.MaxNumberOfMessages = MaxNumberOfMessages;
+		if (numberOfMessages && typeof numberOfMessages === 'number') {
+			opts.MaxNumberOfMessages = numberOfMessages;
 		}
 
-		const {Messages} = await this.sqs.receiveMessage(opts).promise();
+		const {Messages} = await this.sendCommand(opts, ReceiveMessageCommand);
 		return Messages;
 	}
 
 	/**
-	 *
-	 * @param {string} QueueUrl
-	 * @param {string} ReceiptHandle An identifier associated with the act of receiving the message. A new receipt handle is returned every time you receive a message. When deleting a message, you provide the last received receipt handle to delete the message.
+	 * Deletes the messages in a queue specified by the QueueURL parameter.
 	 */
-	async delete(QueueUrl, ReceiptHandle) {
-		const opts = {
-			QueueUrl,
-			ReceiptHandle
-		};
+	async clean() {
+		const {QueueUrl} = this;
+		await this.sendCommand({QueueUrl}, PurgeQueueCommand);
+	}
 
-		await this.sqs.deleteMessage(opts).promise();
+	/**
+	 * Deletes the queue specified by the QueueUrl, regardless of the queue's contents.
+	 */
+	async destroy() {
+		const {QueueUrl} = this;
+		await this.sendCommand({QueueUrl}, DeleteQueueCommand);
+		console.warn('You must wait 60 seconds after deleting a queue before you can create another queue with the same name.');
 	}
 }
-
--- nodejs/sqs/test/sqsTest.js
+++ nodejs/sqs/test/sqsTest.js
@@ -1,39 +1,48 @@
-import assert from 'assert'
-import {SQS} from '../sqs.js';
+import assert from 'assert';
+import {SQS, Message} from '../sqs.js';
 
-import {sleep} from '@davidkhala/light/index.js'
+import {sleep} from '@davidkhala/light/index.js';
+
 const sqs = new SQS();
 const queue = 'standard-sqs';
 
-describe('sqs', function (){
-	this.timeout(0)
-	it('list', async ()=>{
-		const health = await sqs.ping()
+describe('sqs', function () {
+	this.timeout(0);
+	it('list', async () => {
+		const health = await sqs.ping();
 		assert.ok(health);
+		const q = await sqs.getQueueUrl(queue);
+		console.info(q);
 		const queues = await sqs.list();
-		console.debug(queues)
-	})
-	it('destroy', async ()=>{
+		assert.ok(queues.includes(q));
+	});
+	it('destroy', async () => {
 		let queues = await sqs.list();
-		await sqs.destroy(queues[0]);
+		const message = Message.FromSQS(queues[0], sqs)
+		await message.destroy();
 		queues = await sqs.list();
 		console.debug('immediate after deletion', queues);
 		const sleepTime = 60000;
 		await sleep(sleepTime);
 		queues = await sqs.list();
 		console.debug(`${sleepTime / 1000} seconds after deletion`, queues);
-	})
-	it('message', async ()=>{
+	});
+	it('create', async () => {
 		const QueueUrl = await sqs.create(queue);
-		await sqs.send(QueueUrl, 'messageB');
-		let Messages = await sqs.receive(QueueUrl);
+		console.info(QueueUrl);
+	});
+	it('message', async () => {
+		const message = new Message(queue);
+		await message.send('messageB', 10000);
+		let Messages = await message.receive({timeout: 11000});
 		console.debug(Messages);
+
 		const {ReceiptHandle} = Messages[0];
-		await sqs.delete(QueueUrl, ReceiptHandle);
-		Messages = await sqs.receive(QueueUrl);
+		await message.delete(ReceiptHandle);
+		Messages = await message.receive();
 		console.debug('after delete', Messages);
-	})
-})
+	});
+});
 
 
 
