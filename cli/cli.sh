3d00e905c31d776bc19cb23111b1bcb65d313227
David Liu <david-khala@hotmail.com>
2023-08-28 09:24:25 +0800

    Update cli.sh

--- cli/cli.sh
+++ cli/cli.sh
@@ -1,4 +1,4 @@
-set -e
+et -e
 install() {
 	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 	unzip awscliv2.zip
@@ -10,5 +10,9 @@ uninstall() {
 	sudo rm /usr/local/bin/aws
 	sudo rm /usr/local/bin/aws_completer
 	sudo rm -rf /usr/local/aws-cli
+}
+auth() {
+# Verify your credential
+	aws sts get-caller-identity
 }
 $@
