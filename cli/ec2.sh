4f8133fa5826ad9fc16f7fa48dd06038be2d1d31
David Liu <david-khala@hotmail.com>
2024-06-18 10:02:09 +0800

    Update ec2.sh

--- cli/ec2.sh
+++ cli/ec2.sh
@@ -1,5 +1,4 @@
 describe-public-key(){
-  aws ec2 describe-key-pairs --key-names $1 --include-public-key
-   
+  aws ec2 describe-key-pairs --key-names $1 --include-public-key | jq -r ".KeyPairs[0].PublicKey"
 }
 $@
