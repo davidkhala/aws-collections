3e149e1d68dc4573fa85ea720f8584dce0888ca9
David Liu <david-khala@hotmail.com>
2024-06-18 10:17:08 +0800

    Update ec2.sh

--- cli/ec2.sh
+++ cli/ec2.sh
@@ -1,8 +1,10 @@
+set -e
 describe-public-key(){
   aws ec2 describe-key-pairs --key-names $1 --include-public-key | jq -r ".KeyPairs[0].PublicKey"
 }
 create-key-pair(){
   aws ec2 create-key-pair --key-name $1 --key-type rsa --key-format pem --query "KeyMaterial" --output text > "${1}.pem"
   describe-public-key $1
+  chmod 400 "${1}.pem"
 }
 $@
