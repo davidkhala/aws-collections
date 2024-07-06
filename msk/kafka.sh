c51b9afaa0c88ece1711d936a0278f98dc110b39
David Liu <david-khala@hotmail.com>
2024-07-06 08:52:44 +0800

    Create kafka.sh

---
+++ msk/kafka.sh
@@ -0,0 +1,11 @@
+set -e
+install-client(){
+  # https://docs.aws.amazon.com/msk/latest/developerguide/create-topic.html
+  wget https://archive.apache.org/dist/kafka/${MSK_VERSION}/kafka_2.13-${MSK_VERSION}.tgz
+  tar -xzf kafka_2.13-${MSK_VERSION}.tgz
+  cd kafka_2.13-${MSK_VERSION}/libs
+  wget https://github.com/aws/aws-msk-iam-auth/releases/download/v1.1.1/aws-msk-iam-auth-1.1.1-all.jar
+  cd ../bin
+  # client.properties
+}
+$@
