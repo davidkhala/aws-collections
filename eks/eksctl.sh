3b4bc8268043678129f3ec3cee9a5597e1e7379a
David Liu <david-khala@hotmail.com>
2024-08-09 12:36:59 +0000

    list

--- eks/eksctl.sh
+++ eks/eksctl.sh
@@ -1,8 +1,11 @@
-install(){
+set -e
+install() {
   curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
   sudo mv /tmp/eksctl /usr/local/bin
   eksctl version
 
 }
+list() {
+  eksctl get nodegroup --cluster=$clusterName
+}
 $@
-
