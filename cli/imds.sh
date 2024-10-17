c0f253bec1cf2e08f41298b68f6d8503fc23d2a6
David Liu <david-khala@hotmail.com>
2024-10-17 21:07:59 +0800

    Update imds.sh

--- cli/imds.sh
+++ cli/imds.sh
@@ -1,5 +1,13 @@
 # instance metadata
 list(){
-  TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/
+   curl http://169.254.169.254/latest/meta-data/
+}
+get-ip() {
+  # private ip
+  curl http://169.254.169.254/latest/meta-data/local-ipv4
+}
+get-token(){
+  export TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+  echo "X-aws-ec2-metadata-token: $TOKEN" # header for curl
 }
 $@
